9711f9e1216b0b53813465f9697d3dbeec3fa1f0
Bruno Novo <bruno.novo@concrete.com.br>
2017-06-13 18:42:43 -0300

    Primeiro Script Python

---
+++ script1.py
@@ -0,0 +1,10 @@
+# My First Python script
+import sys              # Load a library module
+
+print("Minha plataforma de trabalho: " + sys.platform)     # String that identifies the kind of computar you are working
+
+print(2 ** 100)         # Raise 2 to a power
+
+x = 'Spam!'
+
+print(x * 8)            # String repetition
