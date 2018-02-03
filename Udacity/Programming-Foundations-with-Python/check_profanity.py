355df8204de5d8ffbe0ed9bf8db67f8d95f7c4fd
Bruno Novo <brunonovo@MacBook-Bruno-Novo.local>
2018-02-02 23:46:44 -0200

    Class 5 -> Curse Finder + Fix Project Path

---
+++ Udacity/Programming-Foundations-with-Python/check_profanity.py
@@ -0,0 +1,32 @@
+from urllib import urlopen
+
+# Python Bult-in Functions --> https://docs.python.org/2/library/functions.html
+
+# WDYL Search Engine --> https://en.wikipedia.org/wiki/WDYL_(search_engine)
+
+# Query String --> https://en.wikipedia.org/wiki/Query_string
+
+
+
+movie_quotes_dir = r"/Users/brunonovo/Projects/python/Udemy/Programming-Foundations-with-Python"
+
+def read_text():
+    quotes = open(movie_quotes_dir + "/movie_quotes.txt")
+    contents_of_file = quotes.read()
+    print(contents_of_file)
+    quotes.close()
+    check_profanity(contents_of_file)
+
+def check_profanity(text_to_check):
+    connection = urlopen("http://www.wdylike.appspot.com/?q=" + text_to_check)
+    output = connection.read()
+    #print(output)
+    connection.close()
+    if "true" in output:
+        print("Profanity Alert!!")
+    elif "false" in output:
+        print("This Document has No Curse Words!!")
+    else:
+        print("Could not Scan The Document Properly")
+
+read_text()
